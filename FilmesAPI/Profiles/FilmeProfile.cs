450df3be64a5d112789227895c6fbdb5ee5f0d0b
matheusgregorio <cxlmo7@gmail.com>
2023-09-05 00:15:39 -0300

    final version

---
+++ FilmesAPI/Profiles/FilmeProfile.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Data.Dtos;
+using FilmesApi.Models;
+using AutoMapper;
+
+namespace FilmesAPI.Profiles
+{
+    public class FilmeProfile : Profile
+    {
+        public FilmeProfile() { 
+        
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<UpdateFilmeDto, Filme>();
+            CreateMap<Filme, UpdateFilmeDto>();
+            CreateMap<Filme, ReadFilmeDto>();
+        }   
+    }
+}
